4cc0b5d2afda5de87a4e3b5deb0a72f2690e3e2f
Robert Chen <robertchen234@gmail.com>
2018-10-22 18:56:07 +0000

    automatically backed up by learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -60,15 +60,15 @@ def get_average_age_for_season(data, season)
   avg = sum / counter
 end
 
-age = 0
-count = 0
-  data[season].each do |hash|
-    hash.each do |k,v|
-      if k == "age"
-        count += 1
-        age += v.to_f
-      end
-    end
-  end
-   answer = (age/count).round
-return answer
+# age = 0
+# count = 0
+#   data[season].each do |hash|
+#     hash.each do |k,v|
+#       if k == "age"
+#         count += 1
+#         age += v.to_f
+#       end
+#     end
+#   end
+#   answer = (age/count).round
+# return answer
