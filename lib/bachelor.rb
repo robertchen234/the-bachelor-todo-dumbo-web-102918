6c43f3a930f697c65b09bbfbb7b1cf85d50c04b5
Robert Chen <robertchen234@gmail.com>
2018-10-22 18:48:51 +0000

    automatically backed up by learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -47,5 +47,9 @@ def get_occupation(data, hometown)
 end
 
 def get_average_age_for_season(data, season)
-  # code here
+  data[season].each do |person|
+    person["age"].each do |num|
+      num.reduce(:+) / num.count
+    end
+  end 
 end
