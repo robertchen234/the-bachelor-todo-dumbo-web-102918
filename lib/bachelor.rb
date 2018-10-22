d6e71e4e5a8576e5a3de5ea0656aa5180c7439b4
Robert Chen <robertchen234@gmail.com>
2018-10-22 18:56:55 +0000

    Done.

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -1,19 +1,61 @@
 def get_first_name_of_season_winner(data, season)
-  # code here
+  data[season].each do |profile|
+    profile.each do |key, val|
+      if val == "Winner"
+        return profile["name"].split.first
+      end 
+    end 
+  end 
 end
 
 def get_contestant_name(data, occupation)
-  # code here
+  data.each do |season, people|
+    people.each do |person|
+      person.each do |info, detail|
+        if detail == occupation
+          return person["name"]
+        end
+      end
+    end 
+  end 
 end
 
 def count_contestants_by_hometown(data, hometown)
-  # code here
+  counter = 0
+  data.each do |season, people|
+    people.each do |person|
+      person.each do |info, detail|
+        if detail == hometown 
+          counter += 1
+        end 
+      end 
+    end 
+  end 
+  counter
 end
 
 def get_occupation(data, hometown)
-  # code here
+  data.each do |season, people|
+    people.each do |person|
+      person.each do |info, detail|
+        if detail == hometown
+          return person["occupation"]
+        end
+      end
+    end 
+  end 
 end
 
 def get_average_age_for_season(data, season)
-  # code here
+  counter = 0
+  sum = 0
+  data[season].each do |profile|
+    profile.each do |key, val|
+      if key == "age"
+        counter += 1
+        sum += val.to_f
+      end
+    end
+  end 
+  avg = (sum / counter).round
 end
