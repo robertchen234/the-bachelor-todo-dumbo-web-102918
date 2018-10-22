9cb1ab45ccdad20425ac41f58108d091d9aa5ae3
Robert Chen <robertchen234@gmail.com>
2018-10-22 18:00:44 +0000

    automatically backed up by learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -9,9 +9,11 @@ def get_first_name_of_season_winner(data, season)
 end
 
 def get_contestant_name(data, occupation)
-  data.values.each do |season, val|
-    if val == "Cruise Ship Singer"
-      return key["name"]
+  data.values.each do |season, infos|
+    infos.each do |info, detail|
+      if detail == "Cruise Ship Singer"
+        return info["name"]
+      end
     end 
   end 
 end
