4da69da59ed8ecd147434c77eb2c5561b3153413
Robert Chen <robertchen234@gmail.com>
2018-10-22 17:49:24 +0000

    automatically backed up by learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -1,8 +1,9 @@
 def get_first_name_of_season_winner(data, season)
-  data[season].each do |key, val|
-    if val == "Winner"
-      return data[season][:"name"].split.first
-    end 
+  data[season].each do |profile|
+    profile.each do |key, val|
+      if val == "Winner"
+        return data[season][:"name"].split.first
+      end 
   end 
 end
 
