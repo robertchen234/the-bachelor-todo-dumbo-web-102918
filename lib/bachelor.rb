69f62f9fb3e6a05a8efbc227e9788df065a637d6
Robert Chen <robertchen234@gmail.com>
2018-10-22 18:04:01 +0000

    automatically backed up by learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -14,6 +14,7 @@ def get_contestant_name(data, occupation)
       person.each do |info, detail|
         if detail == occupation
           return people["name"]
+        end
       end
     end 
   end 
